b100a2b37d51fcbadbaf6d3cdfb7ac62caa3acd9
Shane Freed <shane.freed@gmail.com>
2019-01-21 20:14:45 -0500

    deployed with fixed screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
-import { NgModule, enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppsModule } from './apps/apps.module';
 import { DxBoxModule, DxButtonModule, DxDrawerModule, DxListModule, DxToolbarModule, DxMenuModule } from 'devextreme-angular';
 import { HeaderComponent } from './core/header/header.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   imports: [ BrowserModule, AppsModule,
-    DxBoxModule, DxButtonModule, DxDrawerModule, DxListModule, DxToolbarModule, DxMenuModule, AngularFontAwesomeModule ],
+    DxBoxModule, DxButtonModule, DxDrawerModule, DxListModule, DxToolbarModule, DxMenuModule ],
   declarations: [ AppComponent, HeaderComponent ],
   providers: [ ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
-
-// enableProdMode();
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
